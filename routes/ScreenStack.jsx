6b3fc0ebcadd40335438e67a2d55a6b807822b97
Alistair Sinclair-Smith <ajss90@hotmail.co.uk>
2022-05-05 16:25:16 +0100

    creating a new general screen stack for navigation

---
+++ routes/ScreenStack.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Account, Gallery, Home, Leaderboard, Login, Post, Register, Upload, User, } from "../screens/index";
+import { useAuth } from "../contexts/User";
+
+const Stack = createNativeStackNavigator();
+
+function ScreenStack() {
+  const { currentUser } = useAuth();
+  return currentUser ? (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+      <Stack.Screen name="Leaderboard" component={Leaderboard} options={{ headerShown: false }} />
+      <Stack.Screen name="Upload" component={Upload} options={{ headerShown: false }} />
+      <Stack.Screen name="Account" component={Account} options={{ headerShown: false }} />
+      <Stack.Screen name="Post" component={Post} options={{ headerShown: false }} />
+      <Stack.Screen name="User" component={User} options={{ headerShown: false }} />
+      <Stack.Screen name="Gallery" component={Gallery} options={{ headerShown: false }} />
+    </Stack.Navigator>
+  ) : (
+    <Stack.Navigator>
+      <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+      <Stack.Screen name="Register" component={Register} options={{ headerShown: false }} />
+    </Stack.Navigator>
+  );
+}
+
+export default ScreenStack;
