b6fcd37d7550fab4b0b34559257979bc8a5e00d2
Alistair Sinclair-Smith <ajss90@hotmail.co.uk>
2022-05-06 15:34:44 +0100

    adding models directory with fetchPosts model to acquire data from the posts collection

---
+++ controllers/getPosts.js
@@ -0,0 +1,9 @@
+import fetchPosts from "../models/index";
+
+const getPosts = async (PostsData) => {
+    const Posts = await fetchPosts(PostsData)
+
+    return Posts;
+}
+
+export default getPosts;
