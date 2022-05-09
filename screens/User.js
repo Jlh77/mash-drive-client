6f086aa07578f000cb07552af587a44732e07ec8
apwb89 <apwb89@gmail.com>
2022-05-09 14:15:44 +0100

    not styling but util tiny fix and account page loads that users posts

--- screens/User.js
+++ screens/User.js
@@ -1,7 +1,19 @@
 import { View, Text } from 'react-native'
+import { useEffect } from 'react';
+import { fetchPostsByUid } from '../utils/utils';
 
 const User = ({ route, navigation }) => {
     const { userId } = route.params;
+    console.log(userId, 'userId')
+
+    useEffect(() => {
+        getPost()
+    }, [])
+
+    const getPost = async () => {
+        const posts = await fetchPostsByUid(userId)
+    }
+
     return (
     <View>
         <Text>User id : {userId}s' Page</Text>
