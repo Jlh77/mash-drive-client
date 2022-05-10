f71a714d41b1becf9fb19d8badc69dad669beb31
Alistair Sinclair-Smith <ajss90@hotmail.co.uk>
2022-05-10 16:26:21 +0100

    adding new SearchFeed component to components index file

--- components/index.js
+++ components/index.js
@@ -4,5 +4,6 @@ import Feed from './Feed';
 import Navbar from './Navbar';
 // import CommentList from './CommentList';
 import CommentList from './CommentList';
+import SearchFeed from './SearchFeed';
 
-export { ShortPostCard, SearchSortBar, Feed, Navbar, CommentList };
+export { ShortPostCard, SearchSortBar, Feed, Navbar, CommentList, SearchFeed };
