52c323ffb2189b2d31b7a680c56d714145bc50c3
Alistair Sinclair-Smith <ajss90@hotmail.co.uk>
2022-05-11 14:50:43 +0100

    amending imports to match SearchBar rename

--- components/index.js
+++ components/index.js
@@ -1,9 +1,9 @@
 import ShortPostCard from './ShortPostCard';
-import SearchSortBar from './SearchSortBar';
+import SearchBar from './SearchBar';
 import Feed from './Feed';
 import Navbar from './Navbar';
 // import CommentList from './CommentList';
 import CommentList from './CommentList';
 import SearchFeed from './SearchFeed';
 
-export { ShortPostCard, SearchSortBar, Feed, Navbar, CommentList, SearchFeed };
+export { ShortPostCard, SearchBar, Feed, Navbar, CommentList, SearchFeed };
