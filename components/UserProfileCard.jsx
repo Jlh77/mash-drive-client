feb62a2b31baa42a5c0273125a8c5ab2ed63f1e8
Alistair Sinclair-Smith <ajss90@hotmail.co.uk>
2022-05-11 15:22:14 +0100

    adding new component to display username and avi on user screen

---
+++ components/UserProfileCard.jsx
@@ -0,0 +1,39 @@
+import { View, Text, Image, StyleSheet } from 'react-native';
+
+const UserProfileCard = ({usersData}) => {
+
+    return (
+        <View style={styles.container}>
+            <Image style={styles.image} source={usersData.avatar_url}></Image>
+            <Text style={styles.username}>{usersData.username}</Text>
+        </View>
+    ) 
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+    //   flex: 1,
+    //   backgroundColor: '#F5D349',
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    username: {
+        textAlign: "center",
+        textTransform: "uppercase",
+        fontFamily: '"Times New Roman", Times, serif',
+        fontWeight: "bold",
+        fontSize: 30,
+    },
+    image: {
+      width: 100,
+      height: 100,
+      borderRadius: 1000,
+      alignItems: "center",
+      borderColor: "black",
+      borderStyle: "solid",
+      borderWidth: 1,
+    },
+  });
+
+export default UserProfileCard;
