0174ff9f3ad72937e648bc8a5ed007c429e37ff6
Joe Hawkins <josephlhawkins2@gmail.com>
2022-05-11 10:03:05 +0100

    post.js fix

--- screens/Post.js
+++ screens/Post.js
@@ -5,18 +5,17 @@ import {
   StyleSheet,
   SafeAreaView,
   Image,
-} from 'react-native';
-import { useState, useEffect } from 'react';
+} from "react-native";
+import { useState, useEffect } from "react";
 import {
   getCommentsByPostId,
   fetchPostByPostId,
   fetchUserByUid,
-} from '../utils/utils';
-import { ScrollView } from 'react-native-gesture-handler';
-import CommentForm from '../components/CommentForm';
-import { CommentList } from '../components';
-import { DefaultAvatar, DefaultImg } from '../img/avatar'
-
+} from "../utils/utils";
+import { ScrollView } from "react-native-gesture-handler";
+import CommentForm from "../components/CommentForm";
+import { CommentList } from "../components";
+const postImgPlaceholder = require("../img/defaultImage.jpeg");
 
 const Post = ({ route, navigation }) => {
   const { id } = route.params;
@@ -30,7 +29,7 @@ const Post = ({ route, navigation }) => {
   }, [setCommentData]);
 
   const loadInfo = async (id) => {
-    console.log(commentData, '<====== DATA')
+    console.log(commentData, "<====== DATA");
     try {
       const post = await fetchPostByPostId(id);
       const author = await fetchUserByUid(post.uid);
@@ -46,7 +45,7 @@ const Post = ({ route, navigation }) => {
 
   if (isLoading) {
     <View style={styles.preloader}>
-      <ActivityIndicator size='large'></ActivityIndicator>
+      <ActivityIndicator size="large"></ActivityIndicator>
     </View>;
   }
 
@@ -54,35 +53,45 @@ const Post = ({ route, navigation }) => {
     <SafeAreaView style={styles.safeAreaView}>
       <ScrollView>
         <View style={styles.poster}>
-        <Image
+          <Image
             style={styles.authorAvatar}
-            source={{ uri: postAuthorData.avatar_url }}
+            source={{
+              uri:
+                postAuthorData.avatar_url?.length > 1
+                  ? postAuthorData.avatar_url
+                  : postImgPlaceholder,
+            }}
           ></Image>
-        <Text style={styles.poster_username}>{postAuthorData.username}</Text>
+          <Text style={styles.poster_username}>{postAuthorData.username}</Text>
         </View>
         <View style={styles.mainPostArea}>
+          <View style={styles.postSolo}>
+            <Text style={styles.headerTitle}>{postData.title}</Text>
+            <View style={{ alignItems: "center" }}>
+              <Image
+                style={styles.image}
+                source={{
+                  uri: postData.image_url
+                    ? postData.image_url
+                    : postImgPlaceholder,
+                }}
+              ></Image>
 
-        <View style={styles.postSolo}>
-          <Text style={styles.headerTitle}>{postData.title}</Text>
-          <View style={{alignItems: 'center'}}>
-          <Image
-            style={styles.image}
-            source={{ uri: postData.image_url }}
-          ></Image>
-          
-          <Text>{postData.description}</Text>
-          <Text>Votes: {postData.votes}</Text>
+              <Text>{postData.description}</Text>
+              <Text>Votes: {postData.votes}</Text>
+            </View>
           </View>
+          <View style={styles.comments}>
+            <CommentList
+              commentData={commentData}
+              setCommentData={setCommentData}
+            />
           </View>
-          <View style={styles.comments} >
-          <CommentList commentData={commentData} setCommentData={setCommentData}/>
-          </View>
-
         </View>
       </ScrollView>
-       <View style={styles.commentForm}>
-          <CommentForm postId={postData.id} setCommentData={setCommentData} />
-          </View>
+      <View style={styles.commentForm}>
+        <CommentForm postId={postData.id} setCommentData={setCommentData} />
+      </View>
     </SafeAreaView>
   );
 };
@@ -91,61 +100,58 @@ export default Post;
 
 const styles = StyleSheet.create({
   poster: {
-    backgroundColor: '#A47231',
-    justifyContent: 'center', alignItems: 'center'
+    backgroundColor: "#A47231",
+    justifyContent: "center",
+    alignItems: "center",
   },
   poster_username: {
     textAlign: "center",
-textTransform: 'uppercase',
-fontFamily: '"Times New Roman", Times, serif',
-fontWeight: 'bold',
-fontSize: 30,
+    textTransform: "uppercase",
+    fontFamily: '"Times New Roman", Times, serif',
+    fontWeight: "bold",
+    fontSize: 30,
   },
 
   postSolo: {
-backgroundColor: '#F5D349',
+    backgroundColor: "#F5D349",
   },
   safeAreaView: {
-    backgroundColor: '#6e9176',
+    backgroundColor: "#6e9176",
     flex: 1,
     borderColor: "black",
     borderStyle: "solid",
-
-
-
   },
   mainPostArea: {
     // borderWidth: 4,
-    justifyContent: 'center',
+    justifyContent: "center",
     borderColor: "black",
     borderStyle: "solid",
-borderTopWidth: 1,
-borderBottomWidth: 1,
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
   },
   headerTitle: {
-    textTransform: 'capitalize',
-    textAlign: 'center',
+    textTransform: "capitalize",
+    textAlign: "center",
     fontSize: 40,
   },
   image: {
     height: 200,
     width: 200,
-    justifyContent: 'center',
-
+    justifyContent: "center",
   },
   authorAvatar: {
-    width: 100, height: 100, 
-    borderRadius: 1000,  
-    alignItems: 'center',  
-   borderColor: "black",
-   borderStyle: "solid",
-   borderWidth: 1,
+    width: 100,
+    height: 100,
+    borderRadius: 1000,
+    alignItems: "center",
+    borderColor: "black",
+    borderStyle: "solid",
+    borderWidth: 1,
   },
   container: {
     flex: 1,
     padding: 35,
     borderWidth: 1,
-
   },
   inputGroup: {
     flex: 1,
@@ -164,20 +170,14 @@ borderBottomWidth: 1,
     justifyContent: "center",
   },
   comments: {
-    backgroundColor: '#6e9176',
-  borderColor: "black",
-  borderStyle: "solid",
-
-
-
-
+    backgroundColor: "#6e9176",
+    borderColor: "black",
+    borderStyle: "solid",
   },
 
   commentForm: {
-    backgroundColor: 'white',
-  borderColor: "black",
-  borderStyle: "solid",
-
-  
-  }
+    backgroundColor: "white",
+    borderColor: "black",
+    borderStyle: "solid",
+  },
 });
