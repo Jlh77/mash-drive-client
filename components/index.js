883c6a8e89757de2b21cfb8737f1d1f6368fb111
apwb89 <apwb89@gmail.com>
2022-05-12 14:48:58 +0100

    fixed missing import

--- components/index.js
+++ components/index.js
@@ -6,6 +6,7 @@ import Navbar from './Navbar';
 import CommentList from './CommentList';
 import SearchFeed from './SearchFeed';
 import UpDownVoteButtons from './UpDownVoteButtons';
+import UserProfileCard from './UserProfileCard'
 
 
-export { ShortPostCard, SearchBar, Feed, Navbar, CommentList, SearchFeed, UpDownVoteButtons };
+export { ShortPostCard, SearchBar, Feed, Navbar, CommentList, SearchFeed, UpDownVoteButtons, UserProfileCard };
