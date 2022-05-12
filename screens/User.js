f526a5b2d42e58bc4225d2da8b58c76aa0dc2ba5
Alistair Sinclair-Smith <ajss90@hotmail.co.uk>
2022-05-12 12:55:41 +0100

    added another useEffect to collect users data as well as posts

--- screens/User.js
+++ screens/User.js
@@ -4,6 +4,7 @@ import { fetchPostsByUid, fetchUserByUid } from '../utils/utils';
 import { getPosts } from "../controllers/index";
 import { collection } from "firebase/firestore";
 import { db } from "../firebase.config";
+import { UserProfileCard } from "../components/index";
 
 
 
@@ -13,7 +14,7 @@ const User = ({ route, navigation }) => {
     const [usersPosts, setUsersPosts] = useState([])
     const [usersData, setUsersData] = useState({})
     const [isLoading, setIsLoading] = useState(true)
-  const [username, setUsername] = useState("testUser");
+    const [username, setUsername] = useState("testUser");
     const [posts, setPosts] = useState([]);
     const postsCollection = collection(db, "posts");
 
@@ -47,19 +48,24 @@ const User = ({ route, navigation }) => {
     
 
     return (
-    <View style={{backgroundColor: '#F5D349', flex: 1}}>
-<View style={{ justifyContent: "center", alignItems: "center",}}>
-        <Image style={styles.image} source={usersData.avatar_url}></Image>
-        <Text style={styles.username}>{usersData.username}</Text>
-        </View>
+    // <View style={{backgroundColor: '#F5D349', flex: 1}}>
+    
+    <View style={styles.userContainer}>
+    {/* //   <View style={{ justifyContent: "center", alignItems: "center", backgroundColor: "red"}}>
+    //     <Image style={styles.image} source={usersData.avatar_url}></Image>
+    //     <Text style={styles.username}>{usersData.username}</Text>
+    //   </View> */}
+      <UserProfileCard usersData={usersData} />
 
-        <View style={styles.statsBox}>
+      <View style={styles.statsBox}>
         <Text style={{fontSize: 30}}>Number of Posts: {usersPosts.length}</Text>
         <Text style={{fontSize: 30}}>Reputation: {usersData.reputation}</Text>
         {/* <Text>Number of Comments: {usersData.array_of_comment_ids}</Text> */}
-        </View>
-        <View style={{ alignItems: "center" }}>
-        <View style={[styles.gallery, styles.wireframeBorder]}>
+      </View>
+
+      <View style={{ alignItems: "center" }}>
+        {/* <View style={[styles.gallery, styles.wireframeBorder]}> */}
+        <View style={styles.gallery}>
           <Text style={{ color: "1b242A", fontWeight: "bold", fontSize: 15 }}>
             {" "}
            Newest posts by {usersData.username}{" "}
@@ -75,74 +81,67 @@ const User = ({ route, navigation }) => {
                 ></Image>
               );
             }}
-          ></FlatList>
+          >
+          </FlatList>
         </View>
       </View>
-
-        {/* <View>
-            <Text>{usersData.username}'s Posts</Text>
-            {usersPosts.map(post => {
-                return <TouchableOpacity style={styles.postLink} key={post.id} onPress={() => navigation.navigate('Post', { id: post.id})}>
-                    <Text>{post.title}</Text>
-                    <Text>{post.votes}</Text>
-                </TouchableOpacity> 
-            })}
-        </View> */}
     </View>
     )
 }
 
 const styles = StyleSheet.create({
-
-    username: {
-        textAlign: "center",
-        textTransform: "uppercase",
-        fontFamily: '"Times New Roman", Times, serif',
-        fontWeight: "bold",
-        fontSize: 30,
-    },
-
-    image: {
-            width: 100,
-            height: 100,
-            borderRadius: 1000,
-            alignItems: "center",
-            borderColor: "black",
-            borderStyle: "solid",
-            borderWidth: 1,
-
-    },
-    statsBox: {
-        borderWidth: 1,
-        backgroundColor: '#6e9176',
-        textAlign: 'center',
-    },
-    postLink: {
-        borderWidth: 1,
-        backgroundColor: 'beige'
-    },
-    imageGallery: {
-        height: 115,
-        width: 135,
-        padding: 50,
-        margin: 2,
-    },
-    wireframeBorder: {
-        borderColor: "black",
-        borderStyle: "solid",
-        borderWidth: 1,
-        alignContent: "center",
-      },
-      gallery: {
-        margin: 50,
-        flexDirection: "row",
-        flexWrap: "wrap",
-        justifyContent: "space-evenly",
-        padding: 10,
-        backgroundColor: "#6e9176",
-        width: 300,
-        alignContent: "center",
-      },
+  userContainer: {
+    // flex: 1,
+    backgroundColor: '#F5D349', 
+  },
+  username: {
+      textAlign: "center",
+      textTransform: "uppercase",
+      fontFamily: '"Times New Roman", Times, serif',
+      fontWeight: "bold",
+      fontSize: 30,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 1000,
+    alignItems: "center",
+    borderColor: "black",
+    borderStyle: "solid",
+    borderWidth: 1,
+  },
+  statsBox: {
+      borderWidth: 1,
+      backgroundColor: '#6e9176',
+      textAlign: 'center',
+  },
+  postLink: {
+      borderWidth: 1,
+      backgroundColor: 'beige'
+  },
+  imageGallery: {
+      height: 115,
+      width: 135,
+      padding: 50,
+      margin: 2,
+  },
+  wireframeBorder: {
+    borderColor: "black",
+    borderStyle: "solid",
+    borderWidth: 1,
+    alignContent: "center",
+  },
+  gallery: {
+    flex: 1,
+    margin: 50,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-evenly",
+    padding: 10,
+    backgroundColor: "#6e9176",
+    width: 300,
+    alignContent: "center",
+  },
 })
 
 export default User;
